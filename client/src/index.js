015df4a11fa37cf8d299f7b58f3af100b9f16932
jcariajano <juliocariajano@gmail.com>
2022-07-29 00:09:36 -0500

    II

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux"
 import store from "./store/index"
@@ -7,7 +8,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import dotenv from "dotenv";
 dotenv.config();
-import axios from 'axios'
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 ReactDOM.render(
   <Provider store={store}>
